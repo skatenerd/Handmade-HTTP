2e83574d2158a1d5fa875711e691626ce2e641e9
Wai Lee Chin Feman <wchinfeman@8thlight.com>
2012-02-29 15:59:06 -0600

    clean up interface

--- src/StartServer.java
+++ src/StartServer.java
@@ -12,15 +12,33 @@ import java.util.List;
  * To change this template use File | Settings | File Templates.
  */
 public class StartServer {
+    private List<ResponseSubsystem> _subsystems=null;
+    public StartServer(List<ResponseSubsystem> subsystems){
+        _subsystems = subsystems;
+    }
+
     public static void main(String[] args)
             throws IOException {
+
+        StartServer starter=new StartServer(null);
+        starter.parseArgsAndStartServer(args);
+    }
+    
+    public void parseArgsAndStartServer(String [] args)
+    throws IOException{
         CommandLineParser parser = new CommandLineParser(args, new FileBrowserImpl());
         if (parser.isValidInput()) {
-            startServer(parser.port(), parser.path(), ConfigConstants.getDefaultSubsystems(parser.path(), parser.port()));
+            if(_subsystems==null){
+                _subsystems=ConfigConstants.getDefaultSubsystems(parser.path(), parser.port());
+            }
+            startServer(parser.port(), parser.path(), _subsystems);
         } else {
             System.out.println("Invalid inputs please try again");
         }
     }
+    
+    
+    
     public static void startServer(int port, String path,List<ResponseSubsystem> subsystems)
     throws IOException {
         Server server = new Server(path,port,subsystems);
@@ -29,5 +47,6 @@ public class StartServer {
         System.in.read();
         server.kill();
     }
+    
 
 }
